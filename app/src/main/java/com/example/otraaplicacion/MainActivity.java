e3032531619f375ae28e555c475cd761504f177c
Victoria Vidana <devany1905@hotail.com>
2024-09-18 20:14:18 -0600

    se agrego metodo para calcular la edad

--- app/src/main/java/com/example/otraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/otraaplicacion/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.otraaplicacion;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        EditText edadedit = findViewById(R.id.edad_id);
+        TextView resulttext = findViewById(R.id.resultado_texto);
+        Button button = findViewById(R.id.boton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String edad = edadedit.getText().toString();
+                int edadint = Integer.parseInt(edad);
+                int resultado = edadint*7;
+                String resultadostring = "La edad del canino es: " + resultado + " años";
+                resulttext.setText(resultadostring);
+            }
         });
     }
 }
